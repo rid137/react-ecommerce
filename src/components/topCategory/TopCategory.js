dd597bf6714b5932ae5d4ed40db414197804f464
roycoMedia <makinderidwan73@gmail.com>
2023-03-04 14:30:17 +0100

    sixth commit

--- src/components/topCategory/TopCategory.js
+++ src/components/topCategory/TopCategory.js
@@ -43,12 +43,12 @@ const TopCategory = () => {
             <p className="border-b-4 border-red-500 pb-1 md:pb-6">Top Category</p>
         </div>
 
-        <div className="flex flex-wrap gap-x-4 gap-y-6  mt-6">
+        <div className="flex flex-wrap gap-x-4 gap-y-6 ">
 
         {
                 topCategoryMaleProduct && 
                     topCategoryMaleProduct.map((item) => (
-                        <div key={item.id} className="relative flex flex-col box-border w-72 h-[30rem] bg-white border border-gray-300 p-2 drop-shadow-lg hover:scale-105 transition-all duration-300">
+                        <div key={item.id} className="relative flex flex-col box-border w-72 h-[30rem] mt-6 md:mt-4 bg-white border border-gray-300 p-2 drop-shadow-lg hover:scale-105 transition-all duration-300">
                             {/* <button className="absolute top-0 left-0 text-xl text-white rounded-sm bg-red-600 px-2 pb-1 ">20% off</button> */}
                             <img src={item.thumbnail} alt="" className="h-1/2 object-cover"/>
                             <div className="h-1/2 text-center pt-3">
