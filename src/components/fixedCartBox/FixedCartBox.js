bf8380497379a8eaf308da72f21b7364bb1de22f
roycoMedia <makinderidwan73@gmail.com>
2023-03-04 13:43:08 +0100

    sixth commit

--- src/components/fixedCartBox/FixedCartBox.js
+++ src/components/fixedCartBox/FixedCartBox.js
@@ -9,21 +9,21 @@ const FixedCartBox = () => {
 
   return (
     <div>
-        <div className="fixed z-40 py-1 pb-2 px-1 right-0 top-50 w-32 h-[8.8rem] bg-yellow-500">
+        <div className="fixed z-40 py-1 pb-2 px-1 right-0 top-50 w-20 h-[6rem] md:w-32 md:h-[8.8rem] bg-yellow-500">
             <div className="flex flex-col ">
-                <div className="flex  justify-center text-xl font-bold text-yellow-900">
-                    <p className="mr-1">Your Cart</p>
-                    <BsFillCartCheckFill className="mt-1 text-xl"/>
+                <div className="flex  justify-center text-sm font-bold text-yellow-900">
+                    <p className="text-[.69rem] md:text-xl mr-1">Your Cart</p>
+                    <BsFillCartCheckFill className="mt-1 text-sm md:text-xl"/>
                 </div>
 
-                <div className="flex justify-center text-lg font-bold text-white">
+                <div className="flex justify-center text-sm md:text-lg -mt-1 md:-mt-0 font-bold text-white">
                     <p className="mr-3">Items:</p>
                     <p className="bg-yellow-700 -mt-1 py-1 text-center px-2 rounded-full">{cart.length}</p>
                 </div>
 
-                <div className="flex justify-center">
+                <div className="flex justify-center -mt-4">
                     <Link to={`/cart`} className='no-underline text-white'>
-                        <button className="bg-yellow-700 pt-1 pb-2 px-3 rounded-sm text-md font-bold">View Cart</button>
+                        <button className="bg-yellow-700 pt-1 pb-2 px-3 rounded-sm text-[.5rem] font-sm md:text-md md:font-bold">View Cart</button>
                     </Link>
                 </div>
 
