5c2c2ef9d3776c932895b6ac4a5f4d53103830bf
roycoMedia <makinderidwan73@gmail.com>
2023-04-12 22:04:27 +0100

    commit

---
+++ src/pages/logNreg/LogNReg.js
@@ -0,0 +1,38 @@
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import PropTypes from 'prop-types';
+
+
+import Login from '../login/Login';
+// import ProductDetails from '../productDetails/ProductDetails';
+import Register from '../register/Register';
+
+function LogNReg({setToken}) {
+  return (
+    <Tabs
+      defaultActiveKey="profile"
+      id="justify-tab-example"
+      className="mb-2 mx-7 md:mx-[28.6rem] bg-gray-200"
+      justify
+    >
+      <Tab eventKey="home" title="Login" className=''>
+        <Login setToken={setToken}/>
+      </Tab>
+      <Tab eventKey="profile" title="Register">
+        <Register />
+      </Tab>
+      {/* <Tab eventKey="longer-tab" title="Loooonger Tab">
+        <Login />
+      </Tab>
+      <Tab eventKey="contact" title="Contact" disabled>
+        <Login />
+      </Tab> */}
+    </Tabs>
+  );
+}
+
+LogNReg.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+export default LogNReg;
